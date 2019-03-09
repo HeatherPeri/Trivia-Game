091dd645bcff0af2470a754a8e929ae27747816c
Heather Peri <heatherhperi@gmail.com>
2019-03-09 10:35:40 -0500

    added css and buttons

---
+++ assets/javascript/script.js
@@ -0,0 +1,52 @@
+function check(){
+    
+
+}
+
+var clockRunning = false;
+var time = 0;
+
+setTimeout(thirtySeconds, 1000 * 30);
+window.onload = function thirtySeconds() {
+ 
+  }
+
+  function reset() {
+
+    time = 0;
+    lap = 1;
+    function start() {
+        if (!clockRunning) {
+          $("#start").on("click", function() {
+      
+          }
+          )
+        }
+      }
+
+// var score = 0;
+// for(var i=0; i <questions.length; i++) {
+//     var response = window.prompt(questions[i].prompt);
+//     if(response == questions[i].answer){
+//         score++;
+//     }
+// alert("you got " + score +"/" + questions.length);
+
+// }
+// var questions = [
+// {
+//     prompt: "What famous serial killer dressed as a clown?
+//     \n(a) John Wayne Gacy \n(b) Ted Bundy \n(c) Zodiac Killer",
+//     answer: "a"
+// }
+
+// {
+//     prompt: "How many times did Ted Bundy escape?
+//     \n(a) 1 \n(b) 2 \n(c) 3",
+//     answer: "b"
+// }
+// {
+//     prompt: "Which serial killer was identified this year (2019)?
+//     \n(a) Zodiac \n(b) Dr. No \n(c) Golden State Killer",
+// }];
+
